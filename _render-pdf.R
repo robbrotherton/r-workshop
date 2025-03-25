9aebbbb02c81d01189f3e13c67d18b8388fc9417
Rob Brotherton <robert.brotherton@gmail.com>
2025-03-25 15:28:07 -0400

    adds strategy to render pdf as well as html and reveal

--- _render-pdf.R
+++ _render-pdf.R
@@ -1,9 +1,14 @@
 if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
   quit()
 } else {
-  file.rename(from = "_quarto.yml", to = "_quarto_website.yml")
-  file.rename("_quarto_pdf.yml", "_quarto.yml")
-  on.exit(file.rename("_quarto.yml", "_quarto_pdf.yml"))
-  on.exit(file.rename("_quarto_website.yml", "_quarto.yml"), add = TRUE)
-  quarto::quarto_render(..., as_job = FALSE)
+  message("switching in pdf yml")
+  file.copy(from = "_quarto_pdf.yml", to = "_quarto.yml", overwrite = TRUE)
+  # file.rename(from = "_quarto.yml", to = "_quarto_website.yml")
+  # file.rename("_quarto_pdf.yml", "_quarto.yml")
+  # on.exit(file.copy(from = "_quarto_website.yml", "_quarto.yml", overwrite = TRUE), add = TRUE)
+  message("rendering project as book pdf")
+  quarto::quarto_render(as_job = FALSE)
+
+  message("rendering done, switching the yml file back")
+  file.copy(from = "_quarto_website.yml", "_quarto.yml", overwrite = TRUE)
 }
