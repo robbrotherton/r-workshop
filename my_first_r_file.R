c9a7b157900890f175340cfd1e8c75504f14e4b1
Robert Brotherton <robert.brotherton@gmail.com>
2025-04-28 09:07:09 -0400

    adds r file for download

---
+++ my_first_r_file.R
@@ -0,0 +1,22 @@
+# install a package we'll use later
+install.packages("tidyverse")
+
+
+# see the current 'working directory'
+getwd()
+
+# vectors
+
+1:5
+
+c(1, 2, 3, 4, 5)
+
+
+# coercion
+
+c(TRUE, TRUE, FALSE, FALSE, 1)
+
+
+sum(c(TRUE, TRUE, FALSE, FALSE))
+
+mean(c(TRUE, TRUE, FALSE, FALSE))
